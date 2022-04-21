9aff394b64f5d897be2abe1c890b7994236d2d99
Nate Ebel <n8ebel@goobar.dev>
2022-04-21 01:32:04 -0600

    Add support for adding/displaying list of programming languages

--- kotlin-js/src/main/kotlin/Welcome.kt
+++ kotlin-js/src/main/kotlin/Welcome.kt
@@ -1,27 +1,45 @@
 import csstype.px
 import csstype.rgb
-import react.FC
-import react.Props
+import react.*
 import react.css.css
 import react.dom.html.InputType
+import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.input
-import react.useState
 
 external interface WelcomeProps : Props {
     var name: String
 }
 
+data class WelcomeState(
+    val title: String,
+    val input: String = "",
+    val languages: List<String> = emptyList()
+) : State
+
+
+// Defines a Functional Component using the Kotlin DSL
 val Welcome = FC<WelcomeProps> { props ->
-    var name by useState(props.name)
+
+    // Create a mutable React state that will update UI when modified
+    var state by useState(
+        initialValue = WelcomeState(
+            title = props.name,
+            languages = listOf("Kotlin")
+        )
+    )
+
+    // header
     div {
         css {
             padding = 5.px
             backgroundColor = rgb(8, 97, 22)
             color = rgb(56, 246, 137)
         }
-        +"Hello, $name"
+        +state.title
     }
+
+    // input field
     input {
         css {
             marginTop = 5.px
@@ -29,9 +47,24 @@ val Welcome = FC<WelcomeProps> { props ->
             fontSize = 14.px
         }
         type = InputType.text
-        value = name
+        value = state.input
         onChange = { event ->
-            name = event.target.value
+            state = state.copy(input = event.target.value)
         }
+    }
+
+    // add button
+    button {
+        onClick = {
+            state = state.copy(languages = state.languages + state.input, input = "")
+        }
+        +"Add Programming Language"
+    }
+
+    // language list
+    div {
+        child(ProgrammingLanguagesList.create {
+            languages = state.languages
+        })
     }
 }
